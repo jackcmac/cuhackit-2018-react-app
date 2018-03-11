532c71d5e34554eb8948a12c9d48ef386cef04b3
jackc <jackcmac24@gmail.com>
2018-03-11 00:13:20 -0500

    added recognize and file input 2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,48 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+var AWS = require('aws-sdk');
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.fileName = "";
+    this.recognize = this.recognize.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  recognize() {
+    console.log('print dum dum');
+    console.log(this.fileName);
+    let rekognition = new AWS.Rekognition();
+    rekognition.DetectText(
+      {
+        Bytes: this.fileName,
+        /*Image: {
+          S3Object: {
+            Bucket: "rekognitionnn",
+            Name: "nuclear-codes/source.jpg"
+          }
+        }*/
+      }
+    )
+  }
+
+  handleChange(selectorFiles) {
+    console.log(selectorFiles);
+    this.fileName = selectorFiles[0];
+  }
+
+  performClick(elemId) {
+    var elem = document.getElementById(elemId);
+    this.fileName = elem.value
+    if (elem && document.createEvent) {
+      var evt = document.createEvent("MouseEvents");
+      evt.initEvent("click", true, false);
+      elem.dispatchEvent(evt);
+    }
+  }
+
   render() {
     return (
       <div className="App">
@@ -13,6 +53,10 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        {/*<a type="button" onClick={this.performClick('theFile')} className="btn btn-danger btn-block">UPLOAD</a>
+        <a type="file" ></a> */}
+        <input type="file" id="theFile" onChange={(e) => this.handleChange(e.target.files)} />
+        <a type="button" onClick={this.recognize} className="btn btn-success btn-block">PROCESS</a>
       </div>
     );
   }
